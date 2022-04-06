0fb6088f6fb7b0174f97c8f49bfaf765f6ea9d46
Alex Ellis <alexellis2@gmail.com>
2022-04-06 15:02:51 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Configure our custom init process, change to /sbin/init to 
+# use the one that ships with the rootfs
+
+# Also sets the network IP address statically, via a kernel parameter
+
 sudo curl --unix-socket /tmp/firecracker.socket -i \
       -X PUT 'http://localhost/boot-source'   \
       -H 'Accept: application/json'           \
@@ -8,6 +13,10 @@ sudo curl --unix-socket /tmp/firecracker.socket -i \
             \"kernel_image_path\": \"./vmlinux\", 
             \"boot_args\": \"console=ttyS0 reboot=k panic=1 pci=off init=/init ip=172.16.0.2::172.16.0.1:255.255.255.0::eth0:off\" 
        }"
+       
+# Configure a network device, notice the host_dev_name macthes what 
+# we set in setup_networking.sh
+
 sudo curl -X PUT \
     --unix-socket /tmp/firecracker.socket \
     'http://localhost/network-interfaces/eth0' \
@@ -18,6 +27,7 @@ sudo curl -X PUT \
         "host_dev_name": "ftap0"
     }'
 
+# Configure the first drive, pointing at our disk image
 sudo curl --unix-socket /tmp/firecracker.socket -i \
     -X PUT 'http://localhost/drives/rootfs' \
     -H 'Accept: application/json'           \
@@ -29,6 +39,7 @@ sudo curl --unix-socket /tmp/firecracker.socket -i \
             \"is_read_only\": false 
     }"
 
+# Boot the VM
 sudo curl --unix-socket /tmp/firecracker.socket -i \
     -X PUT 'http://localhost/actions'       \
     -H  'Accept: application/json'          \
